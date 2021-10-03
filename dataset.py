ff2077227e1d2fc60a4de652dca893c40457dea4
JihyunLee <jihyunlee@postech.ac.kr>
2021-10-03 23:57:39 +0900

    change the dataset format

--- dataset.py
+++ dataset.py
@@ -25,26 +25,33 @@ print("Load Tokenizer")
 # }
 
 
+            
+            
 tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased', use_fast=True)
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self,data_name, data_option, tokenizer, max_length, max_options, type):
         self.tokenizer = tokenizer
         self.data_name = data_name
+        self.max_length = max_length
         
 
         try:
+            'a'-1
             print("Load processed data")
             with open(f'data/preprocessed_{type}_{data_name}_{data_option}.pickle', 'rb') as f:
                 encodings = pickle.load(f)
         except:
             print("preprocess data")
             raw_dataset = load_dataset(self.data_name,data_option)
-            contexts, questions_options, labels = self._preprocessing_dataset(raw_dataset[type])
+            input_ids, attention_mask, token_type_ids, labels = self._preprocessing_dataset(raw_dataset[type])
+            tokenized_examples = {
+                'input_ids' : input_ids,
+                'attention_mask' : attention_mask,
+                'token_type_ids' : token_type_ids
+            }
 
             print("Encoding dataset (it will takes some time)")
-            tokenized_examples = tokenizer(contexts, questions_options, max_length = max_length, truncation='only_first', padding=True) # TODO
-
             encodings = {k: [v[i:i+max_options] for i in range(0, len(v), max_options)] for k, v in tokenized_examples.items()}
             encodings['labels'] = labels
             
@@ -67,19 +74,48 @@ class Dataset(torch.utils.data.Dataset):
         return len(self.encodings['input_ids'])
 
     def _preprocessing_dataset(self, dataset):
-        C = []
-        Q_O = []
+        input_idss =[]
+        input_masks = []
+        segment_idss = []
+
         labels = []
         print(f"preprocessing {self.data_name} data")
-        for i, (c, q, os, a) in tqdm(enumerate(zip(dataset['article'], dataset['question'],dataset['options'], dataset['answer'])), total= len(dataset['article'])):
+        for i, (c, q, os, a) in tqdm(enumerate(zip(dataset['article'], dataset['question'],\
+                                    dataset['options'], dataset['answer'])), total= len(dataset['article'])):
             for o in os:
-                C.append(c)
-                Q_O.append(q + ' ' +  o)
+                c_token = tokenizer.tokenize(c)
+                q_token = tokenizer.tokenize(q)
+                o_token = tokenizer.tokenize(o)
+                c_token = self._truncate_c_token(c_token, q_token, o_token)
+                
+                tokens = ["[CLS]"] + c_token + ["[SEP]"] + q_token + ["[SEP]"] + o_token + ["[SEP]"] 
+                segment_ids = [0] * (len(c_token) + 2) + [1] * (len(q_token) + 1)+ [1] * (len(o_token) + 1)
+                input_ids = tokenizer.convert_tokens_to_ids(tokens)
+                input_mask = [1] * len(input_ids)
+
+                # Zero-pad up to the sequence length.
+                padding = [0] * (self.max_length - len(input_ids))
+                input_ids += padding
+                input_mask += padding
+                segment_ids += padding
+                assert self.max_length == len(input_ids) == len(input_mask) == len(segment_ids)
+                input_idss.append(input_ids)
+                input_masks.append(input_mask)
+                segment_idss.append(segment_ids)
+                
             label = ord(a) - ord('A')
-
             labels.append(label)
-
-        return C, Q_O, labels
+        
+        return input_idss, input_masks, segment_idss, labels
+        
+    
+    def _truncate_c_token(self, c,q,o):
+        if len(c) + len(q) + len(o) + 4 > self.max_length:
+            c = c[:self.max_length - (len(q) + len(o) + 4)]
+        assert(len(c) + len(q) + len(o) + 4 <= self.max_length)
+        return c
+            
+        
 
 
 
